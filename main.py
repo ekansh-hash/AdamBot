672c52e78ad6f25a2b45f557e931b2f6551123a9
Ekansh Pali <ekanshpali@gmail.com>
2021-04-18 12:29:42 +0530

    changed get response

--- main.py
+++ main.py
@@ -15,10 +15,21 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='kpop'))
 
+@bot.event
+async def on_message(message):
+   if message.author == bot.user:
+        return
+      
+   WholeMessage = message.content
+
+   if not CanRespond():
+    return 
+   SendMessage = Get_Response(WholeMessage)
+   if SendMessage != "":
+    await message.channel.send(SendMessage)
 
 @bot.command()
 async def addresponse(message , *args):
-  Response 
   await message.channel.send("testing :)") 
 
 bot.run(os.getenv('TOKEN')) 
